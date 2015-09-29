c5719fd169821de6a5da0a8e1ab09d968d0a3e44
Rhys Clement-Evans <rhys.clement-evans@onthebeach.co.uk>
2015-09-29 12:12:41 +0100

    Working with Kevin - end of morning session

--- tyre-pressure/lib/alarm.rb
+++ tyre-pressure/lib/alarm.rb
@@ -1,3 +1,15 @@
 class Alarm
-  # Fill in the implementation here
+  
+  def initialize(sensor, notifier)
+        @sensor = sensor
+    @notifier = notifier
+  end
+
+  def check
+        pressure = @sensor.sample_pressure
+        if pressure < 17.5 || pressure > 21
+      @notifier.out_of_bounds
+        end
+    @notifier.normal_range
+  end
 end
--- tyre-pressure/spec/alarm_spec.rb
+++ tyre-pressure/spec/alarm_spec.rb
@@ -1,4 +1,53 @@
 require 'alarm'
+require 'sensor'
 RSpec.describe "tyre pressure alarm" do
-  it "needs some tests"
+        subject { Alarm.new(sensor, notifier) }
+        let(:sensor) { double("sensor", :sample_pressure => 17.5) }
+  let(:notifier) { double("notifier") }
+
+
+        before do
+    allow( notifier).to receive(:out_of_bounds)
+    allow( notifier).to receive(:normal_range)
+    subject.check
+        end
+        
+  it "initialize's alarm class with sensor value" do
+        subject
+  end
+
+  it "makes sure sensor can call sample pressure" do
+        expect( sensor ).to receive(:sample_pressure)
+        subject.check
+  end
+
+  context "low tyre pressure" do
+        let(:sensor) { double("sensor", :sample_pressure => 17) }
+
+    it "calls out of bounds on notifier" do
+        expect( notifier ).to receive(:out_of_bounds)
+        subject.check
+    end
+
+  end
+
+  context "high tyre pressure" do
+    let(:sensor) { double("sensor", :sample_pressure => 22) }
+
+    it "checks high tyre pressure" do
+      expect( notifier ).to receive(:out_of_bounds)
+      subject.check
+    end
+  end
+
+  context "tyre pressure within range" do
+    let(:sensor) { double("sensor", :sample_pressure => 18) }
+
+    it "checks false response for alarm when pressure in range" do 
+      expect( notifier ).to receive(:normal_range)
+      subject.check
+    end
+  end
+
+
 end
